d7850aaaaeac31a30ca6b9e209b33b9de3b88a0f
asmith26 <asmith26@users.noreply.github.com>
2017-06-08 11:32:51 +0100

    Fixed fish stuff.

--- fish/json2simple.py
+++ fish/json2simple.py
@@ -1,39 +1,36 @@
-import os
 import glob
+import json
 
-map_labels = {"alb": 0, "bet": 1, "dol": 2, "lag": 3, "NoF": 4, "other": 5, "shark": 6, "yft": 7}
+#map_labels = {"alb": 0, "bet": 1, "dol": 2, "lag": 3, "NoF": 4, "other": 5, "shark": 6, "yft": 7}
 map_data_folders = {"alb": "ALB", "bet": "BET", "dol": "DOL", "lag": "LAG", "NoF": "NoF", "other": "OTHER",
                     "shark": "SHARK", "yft": "YFT"}
 
-bounding_data_path_root = '/home/ubuntu/data/fishing/bounding_boxes/'
-training_data_path_root = '/home/ubuntu/data/fishing/train/'
-output_file = open('/home/ubuntu/data/fishing/bounding_boxes/output/output.txt', 'a')
+bounding_data_path_root = '../fish-data/manual_annotations/'
+all_training_data_path_root = '../fish-data/train/'
 
-# loop through files - get class label
-os.chdir(data_path_root)
-for file in list(glob.glob('*.json')):
-    class_ = file.split('_')[0]
-    class_name = str(map_labels[class_])
+with open(all_training_data_path_root+'annotations_simple.txt', 'w') as output_file:
+    # loop through files - get class label
+    for file_path in glob.iglob(bounding_data_path_root+'*.json'):
+        class_ = file_path.split('/')[-1].split('_')[0]
+        class_name = str(map_data_folders[class_])
 
-    # open file to get data
-    with open(bounding_data_path_root + file) as data_file:
-        data = json.load(data_file)
+        # open file to get data
+        with open(file_path, 'r') as data_file:
+            data = json.load(data_file)
 
-        # loop through json data from each file
-        for element in data:
-            filename = element['filename'].split('/')[-1]
-            file_path = training_data_path_root + map_data_folders[class_] + '/' + filename
+            # loop through json data from each file
+            for element in data:
+                filename = element['filename'].split('/')[-1]
+                img_path = all_training_data_path_root + map_data_folders[class_] + '/' + filename
 
-            # if element exists get relevant elements and write to text file
-            if element.get('annotations'):
-                # TODO check these are correct!?
-                xstart = int(element['annotations'][0]['x'])
-                xend = int(element['annotations'][0]['width']) + xstart
-                ystart = int(element['annotations'][0]['y'])
-                yend = int(element['annotations'][0]['height']) + ystart
-                write_string = file_path + ',' + str(xstart) + ',' + str(ystart) + ',' + str(xend) + ',' + str(
-                    yend) + ',' + str(class_name) + '\n'
-                print(write_string)
-                output_file.write(write_string)
-    data_file.close()
-output_file.close()
+                # if element exists get relevant elements and write to text file
+                if element.get('annotations'):
+                    # TODO check these are correct!?
+                    xstart = int(element['annotations'][0]['x'])
+                    xend = int(element['annotations'][0]['width']) + xstart
+                    ystart = int(element['annotations'][0]['y'])
+                    yend = int(element['annotations'][0]['height']) + ystart
+                    write_string = img_path + ',' + str(xstart) + ',' + str(ystart) + ',' + str(xend) + ',' + str(
+                        yend) + ',' + str(class_name) + '\n'
+                    print(write_string)
+                    output_file.write(write_string)
